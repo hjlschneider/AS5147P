eb803dd7d8c9c3d455771bf69e5ce8c51982af6a
hjlschneider <hjlschneider@outlook.com>
2016-08-17 13:07:38 +0200

    Add files via upload

---
+++ example_AS5147P.ino
@@ -0,0 +1,18 @@
+#include <AS5147P.h>
+
+int csn = 10;
+int miso = 74;
+int mosi = 75;
+int sclk = 76;
+
+AS5147P as5147p(csn, mosi, miso, sclk);
+
+void setup() {
+  Serial.begin(9600);
+  as5147p.startup();
+}
+
+void loop() {
+  int data = as5147p.getPosition();
+  printf("%i\r\n",data);
+}
